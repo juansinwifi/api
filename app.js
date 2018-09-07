6e5634382f0fc5ef8bf9567cd8225566076610f5
Juan Sebastian Otero Diaz <jsoterod@refinancia.internal>
2018-09-07 15:54:22 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const Joi = require('joi'); //Validacion de Inputs en el servicio
+const express = require('express');
+const app = express();
+app.use(express.json()); //Lee entradas en formato json
+
+
+/* DENTIX API */
+
+
+/* MANAGMENT SECTION */
+
+
+//'REQUERIMIENTOS' POST Method  
+
+app.post('/api/admin/requirements', (req, res) => {
+    // Recibir Tipo, SMS (Si/No), Respuesta (Si/No), Medio (Email/Fisico)
+    // Tiempos (Inmediato, Apertura, Seguimiento), dias u horas. 
+    const schema = {
+        type: Joi.string().min(3).required(),
+        sms: Joi.boolean().required()
+    };
+
+    const result = Joi.validate(req.body, schema);
+    if (result.error) {
+        res.status(400).send(result.error.details[0].message);
+    }
+
+    const customer = {
+        type: req.body.type,
+        sms: req.body.sms
+    };
+    //customers.push(customer);
+    res.send(customer);
+});
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
