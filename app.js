c09acd3808a982d3a37b44649f4938803aab19c0
Juan Otero <sebastian@b612.cloud>
2018-09-23 21:17:17 -0500

    Tyoifications Service

--- app.js
+++ app.js
@@ -103,8 +103,20 @@ function validateRequiement(requiement) {
 /* TIPIFICACIONES */
 /******************/
 const typifications = [
-    { id: 1, name: 'ACUERDO DE PAGO', custom: 'La la la' },
-    { id: 2, name: 'NOVEDADES MONETARIAS', custom: 'Do Re Mi Fa' }
+    { id: 1, name: 'ACUERDO DE PAGO'},
+    { id: 2, name: 'NOVEDADES MONETARIAS'}
+];
+
+const childTypifications= [
+    { id: 1, idParent: 1, name: 'REDIFERIDO', description: 'Acuerdo de pago en un nuevo numero de cuotas', forms: [{ id: 1, description: 'Numero de Cuotas', type: 2} ,{ id: 2, description: 'Valor Cuota', type: 3}], levels: [ { id: 1, area: 1, user: 2, profile: 2, days: 2, hours: 5 }], maxTime: 21  },
+    { id: 2, idParent: 2, name: 'CAMBIO DE CICLO', description: 'Novedad de nueva fecha de pagos', forms: [{ id: 1, description: 'Nuevas Fechas de Pago', type: 4}] , levels: [ { id: 1, area: 2, user: 1, profile: 3, days: 5, hours: 8 },{ id: 2, area: 1, user: 1, profile: 1, days: 1, hours: 0 }],  maxTime: 56 },
+];
+
+const varTypes = [
+    { id: 1, name: 'texto', type: 'string'},
+    { id: 2, name: 'Numero', type: 'number'},
+    { id: 3, name: 'Moneda', type: 'number'},
+    { id: 4, name: 'Fecha', type: 'date'}
 ];
 
 //'BUSCAR TIPIFICACIONES' GET Method
@@ -112,7 +124,7 @@ app.get('/api/admin/typifications', (req, res) => {
     res.send(typifications);
 });
 
-//'BUSCAR UNA TIPIFICACION ESPECIFICA' GET Method
+//'BUSCAR UNA TIPIFICACION ' GET Method
 app.get('/api/admin/typifications/:id', (req, res) => {
     //Look up the requierement
     //If not existing, return 404 - Not Found
@@ -120,6 +132,194 @@ app.get('/api/admin/typifications/:id', (req, res) => {
     if (!typification) return res.status(404).send('Tipificación no encontrada'); // Error 404 
     res.send(typification);
 });
+
+//'BUSCAR TIPIFICACIONES ESPECIFICAS' GET Method
+app.get('/api/admin/childtypifications', (req, res) => {
+    res.send(childTypifications);
+});
+
+//'BUSCAR UNA TIPIFICACION ESPECIFICA' GET Method
+app.get('/api/admin/childtypifications/:id', (req, res) => {
+    //Look up the Child Typification
+    //If not existing, return 404 - Not Found
+    const child = childTypifications.find(c => c.id === parseInt(req.params.id));
+    if (!child) return res.status(404).send('Tipificación Especifica no encontrada'); // Error 404 
+    res.send(child);
+});
+
+//'BUSCAR TIPOS DE VARIABLES' GET Method
+app.get('/api/admin/vartypes', (req, res) => {
+    res.send(varTypes);
+});
+
+//'BUSCAR UNA TIPO DE VARIABLE' GET Method
+app.get('/api/admin/vartypes/:id', (req, res) => {
+    //Look up the varTypes
+    //If not existing, return 404 - Not Found
+    const varType = varTypes.find(v => v.id === parseInt(req.params.id));
+    if (!varType) return res.status(404).send('Tipo de Variable no encontrada'); // Error 404 
+    res.send(varType);
+});
+
+//'CREAR TIPIFICACIÓN' POST Method
+app.post('/api/admin/typifications', (req, res) => {
+    //Validate Data
+    //If invalid, return 404 - Bad Request
+    const { error } = validateTypifications(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const typification = {
+        id: typifications.length + 1,
+        name: req.body.name
+    };
+    typifications.push(typification);
+    res.send(typification);
+});
+
+//'CREAR TIPIFICACIÓN ESPECIFICAS' POST Method
+app.post('/api/admin/childtypifications', (req, res) => {
+    //Validate Data
+    //If invalid, return 404 - Bad Request
+    const { error } = validateChildTypifications(req.body);
+    if (error) return res.status(400).send('ERROR: ' + error.details[0].message + '. PATH: ' + error.details[0].path);
+
+    let nForms = 0;
+    let nLevels = 0;
+    
+    while (req.body.forms[nForms]){
+        const validateForm = validateForms(req.body.forms[nForms]);
+        if (validateForm.error) return res.status(400).send(validateForm.error.details[0].message + '. PATH: Forms[' + nForms + '] ' + validateForm.error.details[0].path.toString()); 
+        nForms++;
+    }
+
+    while (req.body.levels[nLevels]){
+        const validateLevel = validateLevels(req.body.levels[nLevels]);
+        if (validateLevel.error) return res.status(400).send(validateLevel.error.details[0].message + '. PATH: Levels[' + nLevels + '] ' + validateLevel.error.details[0].path.toString()); 
+        nLevels++;
+    }
+
+    const childTypification = {
+        id: childTypifications.length + 1,
+        idParent: req.body.idParent,
+        name: req.body.name,
+        description: req.body.description,
+        forms: req.body.forms,
+        levels: req.body.levels,
+        maxTime: req.body.maxTime
+    };
+
+    childTypifications.push(childTypification);
+    res.send(childTypification);
+});
+
+//'MODIFICAR TIPIFICACIÓN' PUT Method
+app.put('/api/admin/typifications/:id', (req, res) => {
+    //Look up the data
+    //If not existing, return 404 - Not Found
+    const typification = typifications.find(t => t.id === parseInt(req.params.id));
+    if (!typification) return res.status(404).send('Tipificación no encontrada'); // Error 404 
+
+    //Validate Data
+    //If invalid, return 404 - Bad Request
+    const { error } = validateTypifications(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    //Update Typification
+    typification.name = req.body.name;
+
+    //Return the updated course
+    res.send(typification);
+});
+
+//'MODIFICAR TIPIFICACIÓN ESPECIFICA' PUT Method
+app.put('/api/admin/childtypifications/:id', (req, res) => {
+    //Look up the data
+    //If not existing, return 404 - Not Found
+    const childTypification = childTypifications.find(c => c.id === parseInt(req.params.id));
+    if (!childTypification) return res.status(404).send('Tipificación Especifica no encontrada'); // Error 404 
+
+    //Validate Data
+    //If invalid, return 404 - Bad Request
+    const { error } = validateChildTypifications(req.body);
+    if (error) return res.status(400).send('ERROR: ' + error.details[0].message + '. PATH: ' + error.details[0].path);
+
+    let nForms = 0;
+    let nLevels = 0;
+    
+    while (req.body.forms[nForms]){
+        const validateForm = validateForms(req.body.forms[nForms]);
+        if (validateForm.error) return res.status(400).send(validateForm.error.details[0].message + '. PATH: Forms[' + nForms + '] ' + validateForm.error.details[0].path.toString()); 
+        nForms++;
+    }
+
+    while (req.body.levels[nLevels]){
+        const validateLevel = validateLevels(req.body.levels[nLevels]);
+        if (validateLevel.error) return res.status(400).send(validateLevel.error.details[0].message + '. PATH: Levels[' + nLevels + '] ' + validateLevel.error.details[0].path.toString()); 
+        nLevels++;
+    }
+
+    //Update Child Typification
+    childTypification.idParent = req.body.idParent,
+    childTypification.name = req.body.name,
+    childTypification.description = req.body.description,
+    childTypification.forms = req.body.forms,
+    childTypification.levels = req.body.levels,
+    childTypification.maxTime = req.body.maxTime
+
+    //Return the Typification
+    res.send(childTypification);
+});
+//Funcion de Validación de Campos del Tipificaciones
+function validateTypifications(requiement) {
+
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+
+    return Joi.validate(requiement, schema);
+}
+
+//Funcion de Validación de Campos del Tipificaciones Especificas
+function validateChildTypifications(requiement) {
+
+    //Para los niveles el ID debe ser menos de 10. 
+    const schema = {
+        idParent: Joi.number().min(1).required(),
+        name: Joi.string().min(3).required(),
+        description: Joi.string().required(),
+        forms: Joi.array().items(Joi.object()).min(1).required(),
+        levels: Joi.array().items(Joi.object()).min(1).required(),
+        maxTime: Joi.number().min(1).required()
+    };
+
+    return Joi.validate(requiement, schema);
+}
+
+//Funcion de Validación de Campos del Formulario
+function validateForms(requiement) {
+
+    const schema = {
+        description: Joi.string().min(2).required(),
+        type: Joi.number().min(1).required()
+    };
+
+    return Joi.validate(requiement, schema);
+}
+
+//Funcion de Validación de Campos del Niveles
+function validateLevels(requiement) {
+
+    const schema = {
+        area: Joi.number().required(),
+        user: Joi.number().required(),
+        profile: Joi.number().required(),
+        days: Joi.number().max(24).required(),
+        hours: Joi.number().max(24).required()
+    };
+
+    return Joi.validate(requiement, schema);
+}
+
 
 
 /*********** */
@@ -443,9 +643,9 @@ function validateArea(requiement) {
 /*********/
 
 const users = [
-    {id: 1, active: true, user: 'pf@dentix.co', password: 'HFK99$$e3#', identification: 1019023277, name: 'Pedro Ficticio', email: 'pedrito.ficticio@gmail.com', profile: 'EJECUTOR', area: 'Clinica Cll90', country: 'Colombia' },
-    {id: 2, active: true, user: 'jf@dentix.co', password: 'HFK99$$e3#', identification: 1020023254, name: 'Juan Ficticio', email: 'juanito.ficticio@outlook.com', profile: 'CONSULTA', area: 'Clinica Restrepo', country: 'Colombia' },
-    {id: 3, active: false, user: 'af@dentix.co', password: 'HFK99$$e3#', identification: 1018041188, name: 'Alejandra Ficticia', email: 'aleja.ficticia@gmail.com', profile: 'RADICADOR', area: 'OPERACIONES', country: 'Colombia' }
+    {id: 1, active: true, user: 'pf@dentix.co', password: 'HFK99$$e3#', identification: 1019023277, name: 'Pedro Ficticio', email: 'pedrito.ficticio@gmail.com', profile: [1,2], area: 1, country: 'Colombia' },
+    {id: 2, active: true, user: 'jf@dentix.co', password: 'HFK99$$e3#', identification: 1020023254, name: 'Juan Ficticio', email: 'juanito.ficticio@outlook.com', profile: [2], area: 3, country: 'Colombia' },
+    {id: 3, active: false, user: 'af@dentix.co', password: 'HFK99$$e3#', identification: 1018041188, name: 'Alejandra Ficticia', email: 'aleja.ficticia@gmail.com', profile: [1,2,3], area: 2, country: 'Colombia' }
 ];
 
 //'BUSCAR USUARIOS' GET Method
@@ -527,13 +727,15 @@ function validateUser(requiement) {
         identification: Joi.number().required(),
         name: Joi.string().min(3).required(),
         email: Joi.string().email({ minDomainAtoms: 2 }).required(),
-        profile: Joi.string().min(3).required(),
-        area: Joi.string().min(3).required(),
+        profile:  Joi.array().items(Joi.number()).min(1).required(),
+        area: Joi.number().min(1).required(),
         country: Joi.string().min(3).required()
     };
 
     return Joi.validate(requiement, schema);
 }
+
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
