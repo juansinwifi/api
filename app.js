87711ffd3f784ad94e0c3ce37ddb52bc340cc205
Juan Otero <sebastian@b612.cloud>
2018-09-26 17:02:43 -0500

    Reject Service

--- app.js
+++ app.js
@@ -1141,5 +1141,75 @@ function validateLight(requiement) {
     return Joi.validate(requiement, schema);
 }
 
+/**************************/
+/* rejects */
+/************************/
+
+const rejects = [
+    { id: 1, name: 'No procede' },
+    { id: 2, name: 'Mal radicado' },
+    { id: 3, name: 'Faltan info o soportes' }
+];
+
+//'BUSCAR Causal de Rechazo' GET Method
+app.get('/api/admin/rejects', (req, res) => {
+    res.send(rejects);
+});
+
+
+//'BUSCAR UN Causal de Rechazo ESPECIFICO' GET Method
+app.get('/api/admin/rejects/:id', (req, res) => {
+    //Look up the requierement
+    //If not existing, return 404 - Not Found
+    const reject = rejects.find(r => r.id === parseInt(req.params.id));
+    if (!reject) return res.status(404).send('Causal de Rechazo no encontrada'); // Error 404 
+    res.send(reject);
+});
+
+//'CREAR Causal de Rechazo' POST Method
+app.post('/api/admin/rejects', (req, res) => {
+    //Validate Data
+    //If invalid, return 404 - Bad Request
+    const { error } = validatereject(req.body);
+    //if (error) return res.status(400).send(error.details[0].message);
+    if (error) return res.status(400).send('ERROR: ' + error.details[0].message + '. PATH: ' + error.details[0].path);
+
+    const reject = {
+        id: rejects.length + 1,
+        name: req.body.name
+    };
+    rejects.push(reject);
+    res.send(reject);
+});
+
+//'MODIFICAR Causal de Rechazo' PUT Method
+app.put('/api/admin/rejects/:id', (req, res) => {
+    //Look up the requierement
+    //If not existing, return 404 - Not Found
+    const reject = rejects.find(r => r.id === parseInt(req.params.id));
+    if (!reject) return res.status(404).send('rejecto no encontrado'); // Error 404 
+
+    //Validate Data
+    //If invalid, return 404 - Bad Request
+    const { error } = validatereject(req.body);
+    //if (error) return res.status(400).send(error.details[0].message);
+    if (error) return res.status(400).send('ERROR: ' + error.details[0].message + '. PATH: ' + error.details[0].path);
+
+    //Update reject
+    reject.name = req.body.name;
+
+    //Return the updated course
+    res.send(reject);
+});
+
+//Funcion de Validación de Campos de rejects
+function validatereject(requiement) {
+
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+
+    return Joi.validate(requiement, schema);
+}
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
