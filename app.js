a9d59f306afbb61633c367db89c1339847c8e691
Juan Otero <sebastian@b612.cloud>
2018-09-26 13:50:54 -0500

    Case Service

--- app.js
+++ app.js
@@ -784,5 +784,162 @@ function validateUser(requiement) {
 /* CASES */
 /*********/
 
+const cases = [
+    { id: 1, name: 'RECHAZAR- DEVOLVER', description: 'Se devuelve el caso al usuario anterior', rejection: true },
+    { id: 2, name: 'FINALIZAR - AVANZAR', description: 'Avanzar al siguiente nivel, si es el último, se caso se marca como finalizado', rejection: false },
+    { id: 3, name: 'EN GESTION', description: 'Cuando este pedte rsta para gestionar Incluir fecha de seguimiento', rejection: false }
+];
+
+//'BUSCAR CASOS' GET Method
+app.get('/api/admin/cases', (req, res) => {
+    res.send(cases);
+});
+
+//Traer los perfiles del servicio Profiles
+//Traer las areas del servicio Areas
+
+//'BUSCAR UN CASO ESPECIFICO' GET Method
+app.get('/api/admin/cases/:id', (req, res) => {
+    //Look up the requierement
+    //If not existing, return 404 - Not Found
+    const myCase = cases.find(c => c.id === parseInt(req.params.id));
+    if (!myCase) return res.status(404).send('Gestión no encontrada'); // Error 404 
+    res.send(myCase);
+});
+
+//'CREAR CASOS' POST Method
+app.post('/api/admin/cases', (req, res) => {
+    //Validate Data
+    //If invalid, return 404 - Bad Request
+    const { error } = validateCase(req.body);
+    //if (error) return res.status(400).send(error.details[0].message);
+    if (error) return res.status(400).send('ERROR: ' + error.details[0].message + '. PATH: ' + error.details[0].path);
+
+    const myCase = {
+        id: cases.length + 1,
+        name: req.body.name,
+        description: req.body.description,
+        rejection: req.body.rejection
+    };
+    users.push(myCase);
+    res.send(myCase);
+});
+
+//'MODIFICAR CASO' PUT Method
+app.put('/api/admin/cases/:id', (req, res) => {
+    //Look up the requierement
+    //If not existing, return 404 - Not Found
+    const myCase = cases.find(c => c.id === parseInt(req.params.id));
+    if (!myCase) return res.status(404).send('Gestión no encontrado'); // Error 404 
+
+    //Validate Data
+    //If invalid, return 404 - Bad Request
+    const { error } = validateCase(req.body);
+    //if (error) return res.status(400).send(error.details[0].message);
+    if (error) return res.status(400).send('ERROR: ' + error.details[0].message + '. PATH: ' + error.details[0].path);
+
+    //Update AREA
+    myCase.name = req.body.name;
+    myCase.description = req.body.description;
+    myCase.rejection = req.body.rejection;
+
+
+    //Return the updated course
+    res.send(myCase);
+});
+
+//Funcion de Validación de Campos de Casos
+function validateCase(requiement) {
+
+    const schema = {
+        name: Joi.string().min(3).required(),
+        description: Joi.string().min(3).required(),
+        rejection: Joi.boolean().required()
+    };
+
+    return Joi.validate(requiement, schema);
+}
+
+/******************/
+/* COMMUNICATION */
+/****************/
+
+const communications = [
+    { id: 1, name: 'RECHAZAR- DEVOLVER' },
+    { id: 2, name: 'FINALIZAR - AVANZAR' },
+    { id: 3, name: 'EN GESTION' }
+];
+
+//'BUSCAR CASOS' GET Method
+app.get('/api/admin/cases', (req, res) => {
+    res.send(cases);
+});
+
+//Traer los perfiles del servicio Profiles
+//Traer las areas del servicio Areas
+
+//'BUSCAR UN CASO ESPECIFICO' GET Method
+app.get('/api/admin/cases/:id', (req, res) => {
+    //Look up the requierement
+    //If not existing, return 404 - Not Found
+    const myCase = cases.find(c => c.id === parseInt(req.params.id));
+    if (!myCase) return res.status(404).send('Gestión no encontrada'); // Error 404 
+    res.send(myCase);
+});
+
+//'CREAR CASOS' POST Method
+app.post('/api/admin/cases', (req, res) => {
+    //Validate Data
+    //If invalid, return 404 - Bad Request
+    const { error } = validateCase(req.body);
+    //if (error) return res.status(400).send(error.details[0].message);
+    if (error) return res.status(400).send('ERROR: ' + error.details[0].message + '. PATH: ' + error.details[0].path);
+
+    const myCase = {
+        id: cases.length + 1,
+        name: req.body.name,
+        description: req.body.description,
+        rejection: req.body.rejection
+    };
+    users.push(myCase);
+    res.send(myCase);
+});
+
+//'MODIFICAR CASO' PUT Method
+app.put('/api/admin/cases/:id', (req, res) => {
+    //Look up the requierement
+    //If not existing, return 404 - Not Found
+    const myCase = cases.find(c => c.id === parseInt(req.params.id));
+    if (!myCase) return res.status(404).send('Gestión no encontrado'); // Error 404 
+
+    //Validate Data
+    //If invalid, return 404 - Bad Request
+    const { error } = validateCase(req.body);
+    //if (error) return res.status(400).send(error.details[0].message);
+    if (error) return res.status(400).send('ERROR: ' + error.details[0].message + '. PATH: ' + error.details[0].path);
+
+    //Update AREA
+    myCase.name = req.body.name;
+    myCase.description = req.body.description;
+    myCase.rejection = req.body.rejection;
+
+
+    //Return the updated course
+    res.send(myCase);
+});
+
+//Funcion de Validación de Campos de Casos
+function validateCase(requiement) {
+
+    const schema = {
+        name: Joi.string().min(3).required(),
+        description: Joi.string().min(3).required(),
+        rejection: Joi.boolean().required()
+    };
+
+    return Joi.validate(requiement, schema);
+}
+
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
