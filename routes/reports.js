057eb62f40aa9e4ac6e661c7ba31dfa637e61fe6
Juan Otero <sebastian@b612.cloud>
2019-04-08 23:53:21 -0500

    fix

--- routes/reports.js
+++ routes/reports.js
@@ -284,7 +284,7 @@ router.post('/records/opens/', async (req, res) => {
             value: 'reject'
         }
     ];
-    const json2csvParser = new Json2csvParser({ fields, eol });
+    const json2csvParser = new Json2csvParser({ fields, eol: '\r\n' });
     const csv = json2csvParser.parse(response);
     //appReport(csv);
     const random = randomstring.generate(8);
