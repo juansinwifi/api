b76b5923174e89aff599d97c1df79c56f8321902
Juan Otero <sebastian@b612.cloud>
2019-04-29 23:05:29 -0500

    ok

--- routes/flow.js
+++ routes/flow.js
@@ -37,7 +37,7 @@ const appReportUser = require('debug')('app:reportsUser');
 router.get('/:id', async (req, res) => {
     try{
         
-        const flow = await Flow.find({"user": req.params.id, "status": true});
+        const flow = await Flow.find({"user": req.params.id, "status": true}).limit(100);
         if (!flow) return res.status(404).send('Inbox no encontrado'); // Error 404 
         
 
