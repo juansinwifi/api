32c7d4cae2dc21f22d04f037e441180e1e350b76
Juan Otero <sebastian@b612.cloud>
2019-05-02 10:25:55 -0500

    resolve

--- routes/reports.js
+++ routes/reports.js
@@ -802,7 +802,7 @@ router.get('/records/closesweek/', async (req, res) => {
 router.get('/records/closesmonth/', async (req, res) => {
     try {
 
-        const fileName =  './downloads/closemonth.csv';
+        const fileName =  'downloads/closemonth.csv';
         //Creamos un Stream para seguir el archivo y luego borrarlo
         // let file = fs.createReadStream(fileName);
         // file.on('error', function(error){
@@ -810,7 +810,8 @@ router.get('/records/closesmonth/', async (req, res) => {
         //   });
         // res.download(fileName, 'cerrados_ultimo_mes.csv');
         // file.pipe(res);
-        res.sendFile(fileName);
+        var path = require('path');
+        res.sendFile(path.resolve(fileName));
        
     }
     catch(ex){
